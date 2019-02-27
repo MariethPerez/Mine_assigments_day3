305cc72ef6af8975498ca4630ed08f7298bf498c
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-02-27 17:39:42 -0500

    Fix spaces in Binary Search exercise

--- GuessNumber_SearchBinary.rb
+++ GuessNumber_SearchBinary.rb
@@ -1,49 +1,49 @@
 def gues_number_v1(num)
-    finder=false
-    stepCount=0
-    tamaño=16
-    inicioRango=1
-    while  !finder
-        stepCount=stepCount+1
-        mitad=tamaño/2
-        if mitad==0 then
-            mitad=1
-        end
-        finRango=inicioRango+mitad-1
-        for i in inicioRango..finRango
-            if i==num then
-                finder=true
-                break
-            end
-        end
-        tamaño=mitad
-        inicioRango=finRango+1
+  finder = false
+  stepCount = 0
+  tamaño = 16
+  inicioRango = 1
+  while !finder
+    stepCount = stepCount + 1
+    mitad = tamaño / 2
+    if mitad == 0 then
+      mitad = 1
     end
-    puts stepCount
+    finRango = inicioRango + mitad - 1
+    for i in inicioRango..finRango
+      if i == num then
+        finder = true
+        break
+      end
+    end
+    tamaño = mitad
+    inicioRango = finRango + 1
+  end
+  puts stepCount
 end
 
-
+# Second version of binary search
 
 def gues_number_v2(num)
-    finder=false
-    stepCount=0
-    inicioRango=1
-    finRango=16
-    while  !finder
-        stepCount=stepCount+1
-        mitad=(inicioRango+finRango)/2
-        if mitad!=num then
-            if num>mitad then
-                inicioRango=mitad+1
-            else
-                finRango=mitad
-            end
-        else
-            finder=true
-            break
-        end
+  finder = false
+  stepCount = 0
+  inicioRango = 1
+  finRango = 16
+  while !finder
+    stepCount = stepCount + 1
+    mitad = (inicioRango + finRango) / 2
+    if mitad != num then
+      if num > mitad then
+        inicioRango = mitad + 1
+      else
+        finRango = mitad
+      end
+    else
+      finder = true
+      break
     end
-    puts stepCount
+  end
+  puts stepCount
 end
 
 gues_number_v2(3)
